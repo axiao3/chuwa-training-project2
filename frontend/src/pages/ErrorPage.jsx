709d799691311d65dd96dca0c41c77c1d2dace24
axiao3 <axiao3@asu.edu>
2023-07-23 01:54:30 -0700

    potential reuse from project1

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
+export default function ErrorPage() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    // navigate to home page
+    navigate("/items");
+  };
+
+  return (
+    <div className="error-page">
+      <div className="error-container">
+        <AiOutlineExclamationCircle
+          className="error-item"
+          style={{ fontSize: "1000%" }}
+        />
+
+        <h1 aria-label="Error">Ooops, something went wrong!</h1>
+
+        <div>
+          <button
+            className="error-item"
+            style={{
+              color: "white",
+              backgroundColor: "var(--var-button-color)",
+              // width: "10%",
+              // height: "3rem",
+            }}
+            onClick={handleClick}
+          >
+            {" "}
+            Go Home{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
